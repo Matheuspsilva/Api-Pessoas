9bbf4fbbf36ac9ddbbaa34bb9131f8e9db0042c9
Matheus <matheuspsilva29@gmail.com>
2023-01-20 17:39:40 -0300

    Ajuste model Estado

--- src/main/java/com/matheussilvadev/pessoas/model/Estado.java
+++ src/main/java/com/matheussilvadev/pessoas/model/Estado.java
@@ -1,13 +1,18 @@
 package com.matheussilvadev.pessoas.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Estado {
 
 	@EqualsAndHashCode.Include
