6b877638dbe1a48918565e54e6b89fb7dec23686
Matheus <matheuspsilva29@gmail.com>
2023-01-20 17:40:59 -0300

    Criação migração pessoa

---
+++ src/main/resources/db/migration/V001__criacao-inicial.sql
@@ -0,0 +1,12 @@
+create table pessoa (
+	id bigint not null auto_increment,
+	nome varchar(80) not null,
+	data_nascimento datetime not null,
+	
+	endereco_cidade_id bigint,
+	endereco_cep varchar(9),
+	endereco_logradouro varchar(100),
+	endereco_numero varchar(20),
+	
+	primary key (id)
+) engine=InnoDB default charset=utf8;
