b4b2d28a933d417a582002c7b705b79c80111150
Matheus <matheuspsilva29@gmail.com>
2023-01-22 16:57:32 -0300

    Endpoint pessoa atualizar

--- src/main/java/com/matheussilvadev/pessoas/controller/PessoaController.java
+++ src/main/java/com/matheussilvadev/pessoas/controller/PessoaController.java
@@ -2,11 +2,13 @@ package com.matheussilvadev.pessoas.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,7 +24,7 @@ public class PessoaController {
 
 	@Autowired
 	PessoaRepository pessoaRepository;
-	
+
 	@Autowired
 	CadastroPessoaService cadastroPessoaService;
 
@@ -30,20 +32,29 @@ public class PessoaController {
 	public List<Pessoa> listar() {
 		return pessoaRepository.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
 	public Pessoa buscar(@PathVariable Long id) {
 		return cadastroPessoaService.buscarOuFalhar(id);
 	}
-	
+
 	@PostMapping("/")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Pessoa adicionar(@RequestBody Pessoa pessoa) {
 		System.out.println(pessoa.toString());
 		return cadastroPessoaService.salvar(pessoa);
 	}
-	
-	
-	
-	
+
+	@PutMapping("/{id}")
+	public Pessoa atualizar(@PathVariable Long id, @RequestBody Pessoa pessoa) {
+		
+		Pessoa pessoaAtual = cadastroPessoaService.buscarOuFalhar(id);
+
+		BeanUtils.copyProperties(pessoa, pessoaAtual, "id");
+		pessoa.setId(pessoaAtual.getId());
+
+		return cadastroPessoaService.salvar(pessoa);
+
+	}
+
 }
