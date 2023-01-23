30f573b8b19b6d913461f0e12ee11db8c1f20e23
Matheus <matheuspsilva29@gmail.com>
2023-01-23 16:14:10 -0300

    Endpoint pessoa remover

--- src/main/java/com/matheussilvadev/pessoas/api/controller/PessoaController.java
+++ src/main/java/com/matheussilvadev/pessoas/api/controller/PessoaController.java
@@ -1,10 +1,11 @@
-package com.matheussilvadev.pessoas.controller;
+package com.matheussilvadev.pessoas.api.controller;
 
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +56,12 @@ public class PessoaController {
 
 		return cadastroPessoaService.salvar(pessoa);
 
+	}
+	
+	@DeleteMapping("/{pessoaId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long pessoaId) {
+		cadastroPessoaService.excluir(pessoaId);
 	}
 
 }
--- src/main/java/com/matheussilvadev/pessoas/domain/service/CadastroPessoaService.java
+++ src/main/java/com/matheussilvadev/pessoas/domain/service/CadastroPessoaService.java
@@ -1,6 +1,7 @@
 package com.matheussilvadev.pessoas.domain.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.matheussilvadev.pessoas.domain.exception.PessoaNaoEncontradaException;
@@ -13,21 +14,32 @@ public class CadastroPessoaService {
 
 	@Autowired
 	PessoaRepository pessoaRepository;
-	
+
 	@Autowired
 	CidadeService cidadeService;
-	
+
 	public Pessoa salvar(Pessoa pessoa) {
-		
+
 		Long cidadeId = pessoa.getEndereco().getCidade().getId();
-		
+
 		Cidade cidade = cidadeService.buscarOuFalhar(cidadeId);
-		
+
 		pessoa.getEndereco().setCidade(cidade);
 
 		return pessoaRepository.save(pessoa);
 	}
-	
+
+	public void excluir(Long id) {
+		
+		try {
+			pessoaRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new PessoaNaoEncontradaException(id);
+		}
+		
+
+	}
+
 	public Pessoa buscarOuFalhar(Long pessoaId) {
 		return pessoaRepository.findById(pessoaId).orElseThrow(() -> new PessoaNaoEncontradaException(pessoaId));
 	}
